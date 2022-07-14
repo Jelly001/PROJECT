54de5aa2b82e770684a328e6717d51cf39bd9645
Mikhail <mikhaildoroshenko9@gmail.com>
2022-07-14 15:01:08 +0300

    second commit

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,11 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
